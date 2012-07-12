e421e8003859bfbdac339f8773d6d0df2dd32279
huangyingw@linux_programming <huangyingw@gmail.com>
2012-07-11 23:57:33 -0500

    n

--- customers/TraceAdvice1.java
+++ customers/TraceAdvice1.java
@@ -2,17 +2,16 @@ package customers;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
 
 @Aspect
 public class TraceAdvice1 {
-	@Before("execution(* module4.aop.order.AccountService.addAccount(..))")
-	public void tracemethodA(JoinPoint joinpoint) {
-		System.out.println("TraceAdvice1:tracemethodA");
-	}
-
-	@Before("execution(* module4.aop.order.AccountService.addAccount(..))")
-	public void tracemethodB(JoinPoint joinpoint) {
-		System.out.println("TraceAdvice1:tracemethodB");
+	@After("execution(* customers.EmailSender.sendEmail(..))&& args(email, message)")
+	public void tracemethodA(JoinPoint joinpoint, String email, String message) {
+		System.out.println(email);
+		System.out.println(joinpoint.getSignature().getName());
+		System.out.println(message);
+		// joinpoint.getTarget().getClass().getMethod("getOutgoingMailServer",
+		// parameterTypes)
 	}
 }
